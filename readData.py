6fcbfbc776581e494c160e4a2781848334c2a028
Martin Li (ml328) <ml328@duke.edu>
2018-02-23 15:41:33 -0500

    added functionality to read in a time and voltage column

--- readData.py
+++ readData.py
@@ -1,20 +1,33 @@
 class readData():
 
     def __init__(self,csvFileName):
-        self.csvFileName = None
+        self.csvFileName = csvFileName
         self.time = None
         self.voltage = None
         self.columnTime = 1
         self.columnVoltage = 2
+        self.checkTimeNan()
 
     @property
     def time(self):
         return self.__time
-    # def voltage(self):
-    #     return self.__voltage
 
     @time.setter
     def time(self,time):
         import pandas as pd
-        reader = pd.read_csv(file, header=None)
-        time = reader.values[1]
+        timeCol = pd.read_csv(self.csvFileName, header=None, usecols=[0])
+        self.__time = timeCol
+        self.checkTimeNaN()
+
+    def checkTimeNaN(self):
+        for 
+
+    @property
+    def voltage(self):
+        return self.__voltage
+
+    @voltage.setter
+    def voltage(self,voltage):
+        import pandas as pd
+        voltageCol = pd.read_csv(self.csvFileName, header=None, usecols=[1])
+        self.__voltage = voltageCol
