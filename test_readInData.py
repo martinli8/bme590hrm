3034a7a014ce8e7fa4584e06ca023796c51dbe94
Martin Li (ml328) <ml328@duke.edu>
2018-02-24 15:18:56 -0500

    added first test to read in faulty data to check for NaN

---
+++ test_readInData.py
@@ -0,0 +1,9 @@
+import pytest
+
+def test_faulty_data_load():
+    from readData import readData
+    myDataset1 = readData("test_data28.csv")
+    nanValueTime1 = 0.9
+    nanValueVoltage1 = -0.345
+    assert myDataset1.time[324] == nanValueTime1
+    assert myDataset1.voltage[338] == nanValueVoltage1
