c11aa75ff600bbd8cf260fcd8709982aa7928bf8
Martin Li (ml328) <ml328@duke.edu>
2018-02-25 20:39:34 -0500

    added autocorrelation functionality in code with dc offset substraction  (-mean)

--- hrmData.py
+++ hrmData.py
@@ -15,15 +15,19 @@ class hrmData():
     @mean_hr_bpm.setter
     def mean_hr_bpm(self,mean_hr_bpm):
         import numpy as np
+        meanVal = np.mean(self.rawData.voltage)
+        self.rawData.voltage[:] = [ x - meanVal for x in self.rawData.voltage]
         a = self.autocorr()
-        corr2csv(a)
+        self.corr2csv(a)
 
     def autocorr(self):
         import numpy as np
+        import math
         # scaledVoltageData = self.rawData.voltage
         # print(scaledVoltageData)
         # #
-        result = np.correlate(self.rawData.voltage, self.rawData.voltage, mode='full')
+        result = np.correlate(self.rawData.voltage, self.rawData.voltage, mode='same')
+        result = result[math.ceil(len(result)/2):]
         return result;
 
         ## code taken from https://stackoverflow.com/
@@ -31,5 +35,9 @@ class hrmData():
         #676302
 
     def corr2csv(self,a):
-        df = pd.Dataframe(a,columns = ["column"])
-        df.to_csv('list.csv',index = False)
+        import matplotlib.pyplot as plt
+        import pandas as pd
+        df = pd.DataFrame(a)
+        df.to_csv('list.csv',index = False,header = False)
+        plt.plot(a)
+        plt.show()
