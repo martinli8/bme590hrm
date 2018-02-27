175306ca2bba7d69cad2909549d5baf15981bc14
Martin Li (ml328) <ml328@duke.edu>
2018-02-27 11:18:41 -0500

    able to determine mean HR over a given interval

--- hrmData.py
+++ hrmData.py
@@ -1,87 +1,110 @@
 class hrmData():
 
-    def __init__(self,readDataClass,intervalStart = None,intervalEnd = None):
+    def __init__(self, readDataClass, intervalStart=None, intervalEnd=None):
 
         self.rawData = readDataClass
 
-        self.intervalStart = intervalStart
-        self.intervalEnd = intervalEnd
-        self.timeSegment = 2;
-
+        self.intervalStart = intervalStart if intervalStart is not None else 0
+        self.intervalEnd = intervalEnd if intervalEnd is not None  \
+                            else self.rawData.time[-1]
+        self.timeSegment = 2
         self.mean_hr_bpm = None
         self.voltage_extremes = None
-        self.duration = None
+        self.duration = self.rawData.time[-1]
         self.num_beats = None
         self.beats = None
         self.adjVol = None
-
 
     @property
     def mean_hr_bpm(self):
         return self.__mean_hr_bpm
 
     @mean_hr_bpm.setter
-    def mean_hr_bpm(self,mean_hr_bpm):
+    def mean_hr_bpm(self, mean_hr_bpm):
         import numpy as np
         import matplotlib.pyplot as plt
-        from timeSegment import timeSegment
 
         meanSubtractedVoltage = []
         meanVal = np.mean(self.rawData.voltage)
-        meanSubtractedVoltage[:] = [ x - meanVal for x in self.rawData.voltage]
+        meanSubtractedVoltage[:] = [x - meanVal for x in self.rawData.voltage]
         self.adjVol = self.smoothVoltage(meanSubtractedVoltage)
         autoCorrelationData = self.autocorr(meanSubtractedVoltage)
-        mySplitVoltages = timeSegment(self.rawData)
-        print(mySplitVoltages.segmentList)
-        lagTime = self.determineLagTime(autoCorrelationData)
+        lagTimes = self.determineLagTime(autoCorrelationData)
+        # print(lagTimes)
+        self.intervalHR(lagTimes)
         self.visualizeData(autoCorrelationData)
 
-
-    def smoothVoltage(self,data):
+    def smoothVoltage(self, data):
         from scipy.signal import savgol_filter
         import matplotlib.pyplot as plt
         import warnings
-        warnings.filterwarnings(action="ignore", module="scipy", \
-            message="^internal gelsd")
-        return savgol_filter(data,11,5)
+        warnings.filterwarnings(action="ignore", module="scipy",
+                                message="^internal gelsd")
+        return savgol_filter(data, 11, 5)
 
-    def autocorr(self,data):
+    def autocorr(self, data):
         import numpy as np
         import math
-        firstNValuesToSkip = 10;
+        firstNValuesToSkip = 14
         result = np.correlate(data, data, mode='same')
         result = result[math.ceil(len(result)/2):]
         result = result[firstNValuesToSkip:]
-        return result;
+        return result
 
-        ## code taken from https://stackoverflow.com/
-        #questions/643699/how-can-i-use-numpy-correlate-to-do-autocorrelation/
-        #676302
+        # code taken from https://stackoverflow.com/
+        # questions/643699/how-can-i-use-numpy-
+        # correlate-to-do-autocorrelation/676302
 
-    def determineLagTime(self,data):
+    def determineLagTime(self, data):
         heartRateList = []
         import numpy as np
         import math
-        if self.intervalStart == None:
-            ind = np.argmax(data) + 10;
-            trueInd = ind - 10;
+        from timeSegment import timeSegment
+
+        mySplitVoltages = timeSegment(self.rawData)
+        # print(mySplitVoltages.segmentList)
+
+        # if self.intervalStart == None:
+        #     ind = np.argmax(data) + 14;
+        #     trueInd = ind - 14;
+        #     mymax = np.amax(data)
+        #     timeAtMax = self.rawData.time[trueInd]
+        #     print(timeAtMax)
+        #     heartRateOverAllTime = 60/timeAtMax
+        #     self.__mean_hr_bpm = heartRateOverAllTime
+
+        # print(heartRateOverAllTime)
+        # print(timeAtMax)
+
+        # else:
+        for i in mySplitVoltages.segmentList:
+            ind = np.argmax(data) + 14
+            trueInd = ind - 14
             mymax = np.amax(data)
             timeAtMax = self.rawData.time[trueInd]
             heartRateOverAllTime = 60/timeAtMax
-            self.__mean_hr_bpm = heartRateOverAllTime
-
-            print(heartRateOverAllTime)
-            print(timeAtMax)
-
-        # else:
-        #     for i in
-            self.__mean_hr_bpm = 20
+            heartRateList.append(heartRateOverAllTime)
+        # print(heartRateList)
+        return heartRateList
 
 
-    def visualizeData(self,a):
+    def intervalHR(self,lagTimes):
+        import numpy as np
+        startIdx = self.convertTimeToIdx(self.intervalStart)
+        endIdx = self.convertTimeToIdx(self.intervalEnd)
+        self.__mean_hr_bpm = np.mean(lagTimes[startIdx:endIdx])
+
+
+    def convertTimeToIdx(self,time):
+        import math
+        idx = math.floor(math.floor(time)/self.timeSegment)
+        return idx
+
+
+    def visualizeData(self, a):
         import matplotlib.pyplot as plt
         import pandas as pd
         df = pd.DataFrame(a)
-        df.to_csv('list.csv',index = False,header = False)
+        df.to_csv('list.csv', index=False, header=False)
         plt.plot(a)
         plt.show()
--- main.py
+++ main.py
@@ -2,12 +2,13 @@ def main():
     from readData import readData
     from hrmData import hrmData
     from timeSegment import timeSegment
-    myDataset = readData("test_data2.csv")
+    myDataset = readData("test_data5.csv")
     # myTimePoints = timeSegment(myDataset, 4)
     # print(myTimePoints.segmentList)
     # print(myTimePoints.intervalStart)
-    hrmObject = hrmData(myDataset)
-    # print(hrmObject.mean_hr_bpm)
+    hrmObject = hrmData(myDataset,0,10)
+    # print(hrmObject.intervalEnd)
+    print(hrmObject.mean_hr_bpm)
     # print(hrmObject.interval)
     # print(hrmObject.rawData.time[324].type)
     # print(myDataset.time[324])
