4dd6fe346514bc10b81fd2e84ab72d8900146a8b
Martin Li (ml328) <ml328@duke.edu>
2018-02-28 11:38:59 -0500

    added untested docstrings and pep8 as well as some csv files to run travis on

--- readData.py
+++ readData.py
@@ -1,5 +1,18 @@
 class readData():
+    """This is a readData class. It stores the data read in from the CSV.
 
+    Attributes:
+        :csvFileName (string): String with the CSV File name that is read in
+
+        :time (list): list of the times read in from the CSV
+
+        :voltage (list): list of the voltages read in from the CSV
+
+        :columnTime (int): The column where the time is located in the CSV
+
+        :columnVoltage (int): The column where the voltage is placed in the CSV
+
+    """
 
     def __init__(self, csvFileName):
         self.csvFileName = csvFileName
@@ -15,6 +28,14 @@ class readData():
 
     @time.setter
     def time(self, time):
+        """
+        Sets the time attribute by reading in the values, appending to a list,
+        checking for NaNs and interpolates accordingly, and returns the list.
+
+        :param self: readData class
+        :returns: the time as an attribute to the readData Class
+        """
+
         import pandas as pd
         timeList = []
         timeCol = pd.read_csv(self.csvFileName, header=None, usecols=[0])
@@ -24,6 +45,13 @@ class readData():
         self.checkTimeNaN()
 
     def checkTimeNaN(self):
+        """
+        Corrects any "NaN"s or blanks in the actual data when read in
+
+        :param self: readData class
+        :returns: the time in the ReadData Class, Nans corrected
+        """
+
         import numpy as np
         a = np.empty(1)
         a[:] = np.nan
@@ -38,6 +66,15 @@ class readData():
 
     @voltage.setter
     def voltage(self, voltage):
+        """
+        Sets the voltage attribute by reading in the values,
+        appending to a list, checking for NaNs and values out of Range,
+        and returns the list.
+
+        :param self: readData class
+        :returns: the voltage as an attribute to the readData Class
+        """
+
         import pandas as pd
         voltList = []
         voltageCol = pd.read_csv(self.csvFileName, header=None, usecols=[1])
@@ -48,6 +85,13 @@ class readData():
         self.checkOutOfECGRange()
 
     def checkVoltageNan(self):
+        """
+        Corrects any "NaN"s or blanks in the actual data when read in
+
+        :param self: readData class
+        :returns: the time in the ReadData Class, Nans corrected
+        """
+
         import numpy as np
         b = np.empty(1)
         b[:] = np.nan
@@ -57,6 +101,13 @@ class readData():
                     self.__voltage[i-1] + self.__voltage[i+1])/2
 
     def checkOutOfECGRange(self):
+        """
+        Checks for any values out of the 300mV ecg range
+
+        :param self: readData class
+        :returns: A warning if value is outside range
+        """
+
         import warnings
         import numpy as np
         import logging
