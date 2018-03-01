aec0ef77c4fb84632bb430f9014fbc35648f2c39
Martin Li (ml328) <ml328@duke.edu>
2018-03-01 12:32:41 -0500

    now catches bad data strings

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 def main():
     from readData import readData
-    myDataset = readData("test_data31.csv")
-    print(myDataset.time[38])
-    print(myDataset.voltage[338])
+    myDataset = readData("test_data30.csv")
+    # print(myDataset.time.type())
+    if (myDataset.time[965] == "bad data"):
+        print("you got got")
+    print(myDataset.time[965])
+    # print(myDataset.time[965].type())
+    # print(myDataset.voltage[338])
     # print(myDataset.voltage)
 
 
