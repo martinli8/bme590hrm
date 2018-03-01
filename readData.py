ff5e8f08f4d6173173df67d464f0d1ad541d20e7
Martin Li (ml328) <ml328@duke.edu>
2018-03-01 14:21:10 -0500

    added json unit test

--- readData.py
+++ readData.py
@@ -1,4 +1,18 @@
 class readData():
+    """This is a readData class. It stores the data read in from the CSV.
+
+    Attributes:
+        :csvFileName (string): String with the CSV File name that is read in
+
+        :time (list): list of the times read in from the CSV
+
+        :voltage (list): list of the voltages read in from the CSV
+
+        :columnTime (int): The column where the time is located in the CSV
+
+        :columnVoltage (int): The column where the voltage is placed in the CSV
+
+    """
 
     def __init__(self, csvFileName):
         self.csvFileName = csvFileName
@@ -14,6 +28,14 @@ class readData():
 
     @time.setter
     def time(self, time):
+        """
+        Sets the time attribute by reading in the values, appending to a list,
+        checking for NaNs and interpolates accordingly, and returns the list.
+
+        :param self: readData class
+        :returns: the time as an attribute to the readData Class
+        """
+
         import pandas as pd
         timeList = []
         timeCol = pd.read_csv(self.csvFileName, header=None, usecols=[0])
@@ -21,22 +43,26 @@ class readData():
             timeList.append(row[0])
         self.__time = timeList
         self.checkTimeNaN()
-        # self.convert_to_native_python_dtype()
 
     def checkTimeNaN(self):
+        """
+        Corrects any "NaN"s or blanks or "bad data" in the actual
+        data when read in
+
+        :param self: readData class
+        :returns: the time in the ReadData Class, Nans corrected
+        """
+
         import numpy as np
         a = np.empty(1)
+        b = "bad data"
         a[:] = np.nan
+        # print(a[0].type)
+        self.__time = [i if i != "bad data" else a[0] for i in self.__time]
         for i, rows in enumerate(self.__time):
             if (str(self.__time[i]) == str(a[0])):
-                self.__time[i] = (self.__time[i-1] + self.__time[i+1])/2
-
-    def convert_to_native_python_dtype(self):
-        import numpy
-        regularList = [];
-        for i in self.__time:
-            regularList.append(numpy.asscalar(i))
-        self.__time = regularList
+                self.__time[i] = (
+                    float(self.__time[i-1]) + float(self.__time[i+1]))/2
 
     @property
     def voltage(self):
@@ -44,6 +70,15 @@ class readData():
 
     @voltage.setter
     def voltage(self, voltage):
+        """
+        Sets the voltage attribute by reading in the values,
+        appending to a list, checking for NaNs and values out of Range,
+        and returns the list.
+
+        :param self: readData class
+        :returns: the voltage as an attribute to the readData Class
+        """
+
         import pandas as pd
         voltList = []
         voltageCol = pd.read_csv(self.csvFileName, header=None, usecols=[1])
@@ -51,25 +86,38 @@ class readData():
             voltList.append(row[0])
         self.__voltage = voltList
         self.checkVoltageNan()
-        # self.convert_to_native_python_dtype2()
+        # self.checkOutOfECGRange()
 
     def checkVoltageNan(self):
+        """
+        Corrects any "NaN"s or blanks in the actual data when read in
+
+        :param self: readData class
+        :returns: the time in the ReadData Class, Nans corrected
+        """
+
         import numpy as np
         b = np.empty(1)
         b[:] = np.nan
+        self.__voltage = [i if i != "bad data"
+                          else b[0] for i in self.__voltage]
         for i, rows in enumerate(self.__voltage):
             if (str(self.__voltage[i]) == str(b[0])):
                 self.__voltage[i] = (
-                    self.__voltage[i-1] + self.__voltage[i+1])/2
+                    float(self.__voltage[i-1]) + float(self.__voltage[i+1]))/2
 
+    def checkOutOfECGRange(self):
+        """
+        Checks for any values out of the 300mV ecg range
 
-    def convert_to_native_python_dtype2(self):
-        import numpy
-        regularList = [];
-        for i in self.__voltage:
-            regularList.append(numpy.asscalar(i))
-        self.__voltage = regularList
-    #
-    # def convertToList(self):
-    #     regularList = [];
-    #     for
+        :param self: readData class
+        :returns: A warning if value is outside range
+        """
+
+        import warnings
+        import numpy as np
+        import logging
+        voltageThreshold = 300
+        outsideRange = [i for i in self.__voltage if (i >= voltageThreshold)]
+        if len(outsideRange) > 0:
+            warnings.warn("outside of normal ECG range!", UserWarning)
---
+++ timeSegment.py
@@ -0,0 +1,105 @@
+class timeSegment():
+
+    """This is a timeSegment class.
+
+    Attributes:
+        :rawData (readData): readData class that contains time and voltage
+
+        :timeSegment (integer): number of seconds to split data segments into
+
+        :listOfSegmentsIdx (list): list of time indices that data is split into
+
+        :segmentList (list): list of list of voltages that are split up into
+        segments
+
+    """
+
+    def __init__(self, readDataClass, timeSegment=None):
+
+        self.rawData = readDataClass
+        self.timeSegment = timeSegment if timeSegment is not None else 2
+        self.listOfSegmentsIdx = None
+        self.segmentList = None
+
+    @property
+    def listOfSegmentsIdx(self):
+        return self.__listOfSegmentsIdx
+
+    @listOfSegmentsIdx.setter
+    def listOfSegmentsIdx(self, listOfSegmentsIdx):
+        """
+        Finds the time index for each segment of X seconds
+
+        :param self: timeSegment Object
+        :returns: listOfSegmentsIdx attribute
+        """
+
+        self.determineTimeIndices()
+
+    def determineTimeIndices(self):
+        """
+        Finds the time index for each segment of X seconds
+
+        :param self: timeSegment Object
+        :returns: listOfSegmentsIdx attribute
+        """
+
+        import math
+        import numpy as np
+        time = self.rawData.time
+        timeValueToMatch = self.timeSegment
+        lastTimePoint = time[-1]
+        lastSegmentTP = math.floor(
+                        math.floor(
+                            lastTimePoint)/self.timeSegment)*self.timeSegment
+
+        indexListOfMatch = []
+
+        while timeValueToMatch <= lastSegmentTP:
+
+            index = (np.abs(np.asarray(time)-timeValueToMatch)).argmin()
+            indexListOfMatch.append(index)
+            timeValueToMatch = timeValueToMatch + self.timeSegment
+
+        self.__listOfSegmentsIdx = indexListOfMatch
+
+    @property
+    def segmentList(self):
+        return self.__segmentList
+
+    @segmentList.setter
+    def segmentList(self, segmentList):
+        """
+        Finds the corresponding voltages fo reach time segment
+
+        :param self: timeSegment Object, segment List Attributes
+        :returns: Segment lists attribute, list of list
+        """
+        self.determineSegments()
+
+    def determineSegments(self):
+        """
+        Finds the voltages for each time segment
+
+        :param self: timeSegment Object
+        :returns: segmentlists attribute, which is a list of lists
+        """
+        mySegmentList = [[] for _ in range(len(self.listOfSegmentsIdx))]
+        voltage = self.rawData.voltage
+        beginningValue = 0
+        nextValue = self.listOfSegmentsIdx[0]
+        voltListToAdd = voltage[beginningValue:nextValue]
+        mySegmentList[0].append(voltListToAdd)
+
+        for counter in range(0, (len(self.listOfSegmentsIdx)-1)):
+            beginningValue = nextValue
+            nextValue = self.listOfSegmentsIdx[counter+1]
+            voltListToAdd2 = voltage[beginningValue:nextValue]
+            mySegmentList[counter+1].append(voltListToAdd2)
+
+        flatList = []
+        for x in mySegmentList:
+            for y in x:
+                flatList.append(y)
+
+            self.__segmentList = flatList
